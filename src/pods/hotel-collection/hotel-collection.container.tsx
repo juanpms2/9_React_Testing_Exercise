73dc5565d18c0082163436eb7dc7aacad9c9fa9f
juanpms2 <jpablo69@hotmail.es>
2020-07-16 08:17:29 +0200

    Añadido hook para evitar memory leak

--- src/pods/hotel-collection/hotel-collection.container.tsx
+++ src/pods/hotel-collection/hotel-collection.container.tsx
@@ -2,11 +2,35 @@ import * as React from 'react';
 import { HotelCollectionComponent } from './hotel-collection.component';
 import { useHotelCollection } from './hotel-collection.hook';
 
+const useSafeState = function <T>(
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const mountedRef = React.useRef(false);
+  const [state, setState] = React.useState<T>(initialValue);
+
+  React.useEffect(() => {
+    mountedRef.current = true;
+    return () => (mountedRef.current = false);
+  }, []);
+
+  const isMounted = () => mountedRef.current;
+
+  const setSafeState = function (
+    data: T
+  ): React.Dispatch<React.SetStateAction<T>> | void {
+    return isMounted() ? setState(data) : null;
+  };
+
+  return [state, setSafeState];
+};
+
 export const HotelCollectionContainer = () => {
   const { hotelCollection, loadHotelCollection } = useHotelCollection();
 
+  const setSafeHotelCollection = () => loadHotelCollection();
+
   React.useEffect(() => {
-    loadHotelCollection();
+    setSafeHotelCollection();
   }, []);
 
   return <HotelCollectionComponent hotelCollection={hotelCollection} />;
