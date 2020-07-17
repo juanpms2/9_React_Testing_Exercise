ceeccb21844384bee92c63ea35ef43712a6bb9a6
juanpms2 <jpablo69@hotmail.es>
2020-07-17 10:23:07 +0200

    Tests en verde, a falta de revisión

--- src/pods/hotel-collection/hotel-collection.hook.ts
+++ src/pods/hotel-collection/hotel-collection.hook.ts
@@ -3,19 +3,15 @@ import { HotelEntityVm } from './hotel-collection.vm';
 import { getHotelCollection } from './hotel-collection.api';
 import { mapFromApiToVm } from './hotel-collection.mapper';
 import { mapToCollection } from 'common/mappers';
-import { useSafeState } from 'common/hooks';
 
 export const useHotelCollection = () => {
-  const [hotelCollection, setHotelCollection] = useSafeState<HotelEntityVm[]>(
+  const [hotelCollection, setHotelCollection] = React.useState<HotelEntityVm[]>(
     []
   );
 
-  const setSafeHotelCollection = (hotelCollection) =>
-    setHotelCollection(hotelCollection);
-
   const loadHotelCollection = () => {
     getHotelCollection().then((result) =>
-      setSafeHotelCollection(mapToCollection(result, mapFromApiToVm))
+      setHotelCollection(mapToCollection(result, mapFromApiToVm))
     );
   };
 
